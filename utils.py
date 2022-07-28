5f04531172a0e90ac28589da7842c674edbca40f
MrSabin <sabin.21011986@gmail.com>
2022-07-28 19:30:43 +0300

    Added requirements.txt

--- utils.py
+++ utils.py
@@ -1,10 +1,11 @@
 import json
 import sqlite3
 
-db = sqlite3.connect("db.sqlite3")
-cursor = db.cursor()
-cursor.execute("SELECT * FROM bot_db_speaker")
-json_string = json.dumps(cursor.fetchall())
-print(json_string)
 
-db.close()
+def db_to_json():
+    db = sqlite3.connect("db.sqlite3")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM bot_db_speaker")
+    json_string = json.dumps(cursor.fetchall())
+    db.close()
+    return json_string
