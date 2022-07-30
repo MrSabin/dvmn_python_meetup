bb18044a29398d221a7217ec7e7fca63ad41dd82
MrSabin <sabin.21011986@gmail.com>
2022-07-30 16:28:10 +0300

    Renamed argument

--- utils.py
+++ utils.py
@@ -2,10 +2,10 @@ import json
 import sqlite3
 
 
-def db_to_json(query):
+def db_to_json(sql_query):
     db = sqlite3.connect("db.sqlite3")
     cursor = db.cursor()
-    cursor.execute(query)
+    cursor.execute(sql_query)
     json_string = json.dumps(cursor.fetchall(), ensure_ascii=False)
     db.close()
     return json_string
