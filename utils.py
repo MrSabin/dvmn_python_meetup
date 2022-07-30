83e52ebbc3fab9b98332b551554683413f710e81
MrSabin <sabin.21011986@gmail.com>
2022-07-30 16:04:17 +0300

    Fixed codepage problems

--- utils.py
+++ utils.py
@@ -6,6 +6,9 @@ def db_to_json():
     db = sqlite3.connect("db.sqlite3")
     cursor = db.cursor()
     cursor.execute("SELECT * FROM bot_db_speaker")
-    json_string = json.dumps(cursor.fetchall())
+    json_string = json.dumps(cursor.fetchall(), ensure_ascii=False)
     db.close()
+    print(json_string)
     return json_string
+
+db_to_json()
